fca26c8c56e9073e08138b4d168006274d37ab25
czelabueno <c.zelabueno@gmail.com>
2020-09-20 23:23:28 -0500

    include static analysis as conditional part of TerratestExecution

--- unit-testing/terratest.go
+++ unit-testing/terratest.go
@@ -1,6 +1,7 @@
 package unit_testing
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -13,22 +14,59 @@ import (
 // t - current test state
 // Return:
 // result - TerraModuleStatus
-func (module *TerraModule) TerratestExecution(t *testing.T) (result TerraModuleStatus) {
+func (module *TerraModule) TerratestExecution(t *testing.T, useStaticAnalysis bool) (result TerraModuleStatus) {
+	terraformOptions, err := CreateTerraformOptions(t, module)
+
+	if err != nil {
+		t.Fatal()
+		return Failed
+	}
+
+	if useStaticAnalysis {
+		module.RunStaticAnalysis(t, terraformOptions)
+		// Run `terraform apply` and failed if en error in founded
+		terraform.Apply(t, terraformOptions)
+	} else {
+		// Run `terraform init` and `terraform apply` and failed if en error in founded
+		terraform.InitAndApply(t, terraformOptions)
+	}
+	// at the end of the test the resources are destroyed
+	defer terraform.Destroy(t, terraformOptions)
+	return Successful
+}
+
+// RunStaticAnalysis ... something comment
+func (module *TerraModule) RunStaticAnalysis(t *testing.T, terraformOptions *terraform.Options) (result TerraModuleStatus) {
+	if terraformOptions == nil {
+		tfOptions, err := CreateTerraformOptions(t, module)
+		terraformOptions = tfOptions
+		if err != nil {
+			t.Fatal()
+			return Failed
+		}
+	}
+	// terraform init ...
+	terraform.Init(t, terraformOptions)
+	// terraform validate ...
+	args := []string{"validate", fmt.Sprintf("-upgrade=%t", terraformOptions.Upgrade)}
+	args = append(args, terraform.FormatTerraformBackendConfigAsArgs(terraformOptions.BackendConfig)...)
+	terraform.RunTerraformCommand(t, terraformOptions, args...)
+	// terraform plan ...
+	defer terraform.Plan(t, terraformOptions)
+	return Successful
+}
+
+// CreateTerraformOptions ... create terraform options with terraform module
+func CreateTerraformOptions(t *testing.T, module *TerraModule) (*terraform.Options, error) {
 	if module.RootFolderPath == "" {
 		module.RootFolderPath = "../" // Set Default root folder path assuming that test file is located in ./test
 	}
 	if module.TerraformModulePath == "" {
 		module.TerraformModulePath = "." // Set Default tf module folder path assuming that .tf files are located in the root
 	}
-	terraformOptions := &terraform.Options{
+
+	return &terraform.Options{
 		TerraformDir: test_structure.CopyTerraformFolderToTemp(t, module.RootFolderPath, module.TerraformModulePath),
 		Vars:         module.Variables,
-	}
-
-	// Run `terraform init` and `terraform apply` and failed if en error in founded
-	terraform.InitAndApply(t, terraformOptions)
-
-	// at the end of the test the resources are destroyed
-	defer terraform.Destroy(t, terraformOptions)
-	return
+	}, nil
 }
