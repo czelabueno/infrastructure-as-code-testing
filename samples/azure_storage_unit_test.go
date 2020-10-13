dfa11d2fb7a3a353eaef03c28bb3c792cbd76a67
czelabueno <c.zelabueno@gmail.com>
2020-10-13 17:39:07 -0500

    updating docs, thoughts and concepts

--- samples/azure_storage_unit_test.go
+++ samples/azure_storage_unit_test.go
@@ -10,17 +10,20 @@ import (
 func Test_storageMod(t *testing.T) {
 	t.Parallel()
 
+	// Define yout terraform input variables
 	vars := make(map[string]interface{})
 	vars["account_tier"] = "Standard"
 	vars["account_replication_type"] = "LRS"
 
+	//Initialize TerraModule struct entering the data to locate individual module that you want test.
 	myunitmodule := unit_testing.TerraModule{
-		RootFolderPath:      "../",
-		TerraformModulePath: "examples/azure/tf-storage",
+		RootFolderPath:      "../",                       //Put root path from this .go file is located
+		TerraformModulePath: "examples/azure/tf-storage", // Put module path where is located .tf files.
 		Variables:           vars,
 	}
 
-	result := myunitmodule.TerratestExecution(t, false)
+	//Call to TerratestExecution to run unit test function: deploy, validate and undeploy.
+	result := myunitmodule.TerratestExecution(t, false) // change to (t, true) to include static analysis
 
 	assert.Equal(t, unit_testing.Successful, result)
 }
